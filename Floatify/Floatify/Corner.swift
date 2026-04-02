bc5cfd77adbce45dd8393fa10099c6c704d30139
Phuoc Hiep <zendragon93@gmail.com>
2026-04-02 18:21:16 +0700

    feat: extend Corner enum with 8 position types and effect/sound defaults
    Add topLeft, topRight, center, menubar, horizontal, cursorFollow positions.
    Each position has defaultEffect and defaultSound computed properties.

--- Floatify/Floatify/Corner.swift
+++ Floatify/Floatify/Corner.swift
@@ -1,6 +1,42 @@
 import Foundation
 
-enum Corner {
+enum Corner: String, CaseIterable {
     case bottomLeft
     case bottomRight
+    case topLeft
+    case topRight
+    case center
+    case menubar
+    case horizontal
+    case cursorFollow
+
+    var defaultEffect: String {
+        switch self {
+        case .bottomLeft, .bottomRight, .topLeft, .topRight:
+            return "slide"
+        case .center:
+            return "fade"
+        case .menubar:
+            return "dropdown"
+        case .horizontal:
+            return "marquee"
+        case .cursorFollow:
+            return "trail"
+        }
+    }
+
+    var defaultSound: String? {
+        switch self {
+        case .bottomLeft, .bottomRight, .topLeft, .topRight:
+            return nil
+        case .center:
+            return "pop"
+        case .menubar:
+            return "tink"
+        case .horizontal:
+            return nil
+        case .cursorFollow:
+            return nil
+        }
+    }
 }
